6d491b39eb508089650156dc01b23a1de2f215ed
Anirban <gangopad@gmail.com>
2015-05-17 19:51:42 -0400

    updated executable.sh

--- executable.sh
+++ executable.sh
@@ -5,12 +5,13 @@ for file in /Users/gangopad/DownloadJournals/PDFs/*.pdf
 do
  # do something on "$file"
  #cat "$file" >> /var/www/cdn.example.com/cache/large.css
- newfile=${file:32}
+ #newfile=${file:32}
+ newfile=file
  #path=/Users/UMBC/MedicalJournals/Texts/
  path=/Users/gangopad/DownloadJournals/Texts
  extension=.txt
  #outfile=$path$newfile$extension
- outfile=file$counter$extension
+ outfile=$path$newfile$counter$extension
  echo $outfile
  python pdf2txt.py "$file" >> $outfile
  counter=$((counter+1))
